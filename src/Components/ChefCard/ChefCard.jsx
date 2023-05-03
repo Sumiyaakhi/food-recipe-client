6b978096cef921fe1afdf2f8ddc4fd4cc6483418
Sumiyaakhi <sumiya.akhi793@gmail.com>
2023-05-03 16:12:54 +0600

    banner added

--- src/Components/ChefCard/ChefCard.jsx
+++ src/Components/ChefCard/ChefCard.jsx
@@ -1,9 +1,28 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 const ChefCard = () => {
+    const [chefCards,setChefCards] = useState([]);
+
+
+    useEffect(()=>{
+        fetch('http://localhost:5000/chefCard')
+        .then(res=> res.json())
+        .then(data => setChefCards(data))
+        .catch(error=> console.error(error))
+    },[])
+
     return (
         <div>
-            
+            <div className="card w-96 bg-base-100 shadow-xl">
+                <figure><img src="/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg" alt="Shoes" /></figure>
+                <div className="card-body">
+                    <h2 className="card-title">Shoes!</h2>
+                    <p>If a dog chews shoes whose shoes does he choose?</p>
+                    <div className="card-actions justify-end">
+                        <button className="btn btn-primary">Buy Now</button>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,20 +1,24 @@
 import React from 'react';
 import './Home.css'
+import ChefCard from '../ChefCard/ChefCard';
 
 const Home = () => {
     return (
         <div>
             {/* Banner section */}
-            <div  className='md:flex justify-between mx-20 my-10'>
+            <div className='md:flex justify-between mx-20 my-10'>
 
-            <div className=''>
-            <h1 className='text-6xl text-amber-500 font-bold pt-8'>Kevin's Famous <br /> Spicy Salsa <br /> with Mangoes!!!</h1>
+                <div className=''>
+                    <h1 className='text-6xl text-amber-500 font-bold pt-8'>Kevin's Famous <br /> Spicy Salsa <br /> with Mangoes!!!</h1>
+                </div>
+                <div>
+                    <img className='rounded' src="https://t3.ftcdn.net/jpg/03/35/51/06/360_F_335510693_HY7mLg3ARdLccKoXk3m66NLDpJRJh51p.jpg" alt="" />
+                </div>
             </div>
+            {/* Chef card section */}
             <div>
-                <img className='rounded' src="https://t3.ftcdn.net/jpg/03/35/51/06/360_F_335510693_HY7mLg3ARdLccKoXk3m66NLDpJRJh51p.jpg" alt="" />
+                <ChefCard></ChefCard>
             </div>
-            </div>
-  {/* Chef card section */}
         </div>
     );
 };
